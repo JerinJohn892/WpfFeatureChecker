b3cbcc9732f30943a4bdcfa179c76bef7a5e0a27
Jerin John <jerinjohn892@outlook.com>
2025-08-15 18:56:55 +0530

    Add project files.

---
+++ WpfCheckerView/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+
+namespace WpfCheckerView
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            try
+            {
+                //Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NHaF5cXmVCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdgWXZfd3RSRGhZVkxxVko="); 23.1.36
+                //Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NMaF5cWWJCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWX5dcHRSQ2hdU0J2WUo="); 28.1.41
+                Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NNaF5cXmBCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWXpfcnRXQmBcWEd1XUBWYUA=");
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error registering Syncfusion license: {ex.Message}", "License Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+
+}
